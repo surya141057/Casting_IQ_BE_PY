5cba412e0c452958bc79dbda09873969e771bca9
surya141057 <suryakrishnamurthy14@gmail.com>
2025-06-22 12:46:29 +0530

    api connect to react

--- castingiq/settings.py
+++ castingiq/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'master',
     'castingiq',
     'common',
@@ -46,6 +47,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',  # ← must come first
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -56,6 +58,13 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'castingiq.urls'
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",  # Vite/React dev server
+]
+CORS_ALLOW_CREDENTIALS = True
+
+
 
 TEMPLATES = [
     {
